1d10e6d8b487f310e73b5b743e1e1df6383a6119
hiro <hi.kanno23@gmail.com>
2020-09-03 14:53:31 +0900

    install mysql and get data

--- api/index.js
+++ api/index.js
@@ -1,19 +1,27 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  const json =  {
-    todo: [
-      {
-        name: 'やること１やることやることやることやることやること',
-        isOpen: true,
-      },
-      {
-        name: 'やること２やることやることやることやることやることやることやることやること',
-        isOpen: false,
-      },
-    ],
-  }
-  res.json(json)
+  const mysql = require('mysql')
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3307',
+    database: 'my_test',
+    user: 'mysql',
+    password: 'mysql',
+  })
+  connection.connect()
+  const sql = 'select * from todo order by create_dt desc, update_dt desc;'
+  connection.query(sql, (error, results, fields) => {
+    if (error) throw error
+    const todo = results.map(result => {
+      return {
+        name: result.task_title,
+        isOpen: result.delete_flg === 0 ? true : false,
+      }
+    })
+    res.json({ todo })
+  })
+  connection.end()
 })
 
 module.exports = {
